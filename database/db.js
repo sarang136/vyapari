c069a477ba6a0f1d1cef761879998a610d65eba7
gopi-gh <gopi-gh@d3vtech.com>
2025-09-15 17:29:17 +0530

    new updates of db

--- database/db.js
+++ database/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const connectDb = async () => {
     try {
-        // const mongoUri = "mongodb+srv://sarang:0vpWoCbrRoi1fRsq@cluster0.au98o.mongodb.net/";
         await mongoose.connect(process.env.MONGODB_URI);
     } catch (error) {
         console.log("database not connected");
